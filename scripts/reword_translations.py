d919aaa459da363c0740623b01238f0016f4973d
Omar Al-Ithawi <i@omardo.com>
2025-02-10 12:54:02 +0300

    feat: support all files

--- scripts/reword_translations.py
+++ scripts/reword_translations.py
@@ -10,6 +10,7 @@ from typing import List
 
 import subprocess
 import sys
+import os
 from pathlib import Path
 import csv
 import re
@@ -56,26 +57,32 @@ class Reword:
 
 def replace_words(s, word, replacement) -> str:
     return re.sub(
-        pattern=rf'\b{word}\b',
+        pattern=r'(?<!\w){word}(?!\w)'.format(word=re.escape(word)),
         repl=replacement,
         string=s,
-        flags=re.IGNORECASE,
+        flags=re.IGNORECASE | re.UNICODE,
     )
 
 
 def contains_word(s, word):
-    return re.search(pattern=rf'\b{word}\b', string=s, flags=re.IGNORECASE)
+    return re.search(
+        pattern=r'(?<!\w){word}(?!\w)'.format(word=word),
+        string=s,
+        flags=re.IGNORECASE | re.UNICODE,
+    )
 
 
 def mk_parents(path: Path):
     path.parent.mkdir(parents=True, exist_ok=True)
 
 
-def get_translation_relative_paths(root_dir: Path):
-    return [
-        path.relative_to(root_dir)
-        for path in root_dir.rglob('*.po')
-    ]
+def get_translation_files_relative_paths(root_dir: Path) -> List[Path]:
+    relative_paths = []
+    for dirpath, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+            file_path = Path(os.path.join(dirpath, filename))
+            relative_paths.append(file_path.relative_to(root_dir))
+    return relative_paths
 
 
 def get_reword_list(reword_csv_file: Path):
@@ -125,7 +132,7 @@ def main(repo_root, *_argv):
     upstream_translations_dir = repo_root / 'translations-upstream'
     updated_translations_dir = repo_root / 'translations'
 
-    translation_paths = get_translation_relative_paths(upstream_translations_dir)
+    translation_paths = get_translation_files_relative_paths(upstream_translations_dir)
 
     for translation_path in translation_paths:
         combined_dest = updated_translations_dir / translation_path
